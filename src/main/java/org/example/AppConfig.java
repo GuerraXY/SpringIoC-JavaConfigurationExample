ca37f7f4377d97e702963fcdd2738285358266ed
Pedro Guerra <pedroguerra@sidero.ie>
2022-10-20 12:54:48 +0100

    Component Scan addition

--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -1,19 +1,12 @@
 package org.example;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "org.example")
 public class AppConfig {
 
-    @Bean
-    public Phone getPhone() {
-        return new Samsung();
-    }
-
-    @Bean
-    public MobileProcessor getProcessor() {
-        return new Snapdragon();
-    }
 
 }
